cdd917fb3bff11aed1fc8e2757b79a732c17985c
Faishal Saiyed <saiyedfaishal@gmail.com>
2016-01-24 04:54:00 +0530

    Fix phpcs issue in header.ph

--- templates/header.php
+++ templates/header.php
@@ -1,18 +1,18 @@
 <header class="banner">
     <div class="container">
-        <a class="brand" href="<?= esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>">
-            <?php if( get_theme_mod( 'site_logo' ) ) { ?>
+        <a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>">
+            <?php if ( get_theme_mod( 'site_logo' ) ) { ?>
                 <img src="<?php echo get_theme_mod( 'site_logo' ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
             <?php } else {
-                bloginfo( 'name' );
-            } ?>
+				bloginfo( 'name' );
+} ?>
         </a>
         <nav class="nav-primary">
             <?php
-            if( has_nav_menu( 'primary_navigation' ) ) :
-                wp_nav_menu( [ 'theme_location' => 'primary_navigation', 'menu_class' => 'nav' ] );
-            endif;
-            ?>
+			if ( has_nav_menu( 'primary_navigation' ) ) :
+				wp_nav_menu( [ 'theme_location' => 'primary_navigation', 'menu_class' => 'nav' ] );
+			endif;
+			?>
         </nav>
         <div class="header-search-wrapper"><?php get_search_form(); ?></div>
     </div>
