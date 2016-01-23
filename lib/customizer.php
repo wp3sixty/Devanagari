3e5bc735ec97aa40d48674270f53e68382d7d470
Sanket <sanket.parmar11@gmail.com>
2016-01-24 03:51:50 +0530

    Header CSS and logo option in customizer

--- lib/customizer.php
+++ lib/customizer.php
@@ -9,6 +9,15 @@ use Roots\Sage\Assets;
  */
 function customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
+
+	$wp_customize->add_setting( 'site_logo' ); // Add setting for logo uploader
+
+	// Add control for logo uploader (actual uploader)
+	$wp_customize->add_control( new \WP_Customize_Image_Control( $wp_customize, 'site_logo', array(
+		'label'    => __( 'Upload Logo (replaces text)', 'devanagari' ),
+		'section'  => 'title_tagline',
+		'settings' => 'site_logo',
+	) ) );
 }
 add_action( 'customize_register', __NAMESPACE__ . '\\customize_register' );
 
