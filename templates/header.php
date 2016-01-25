dc9175bfe953c5b12cbdd11dd06641aa9cf21364
Sanket <sanket.parmar11@gmail.com>
2016-01-26 02:05:06 +0530

    Fixed header css and menu css

--- templates/header.php
+++ templates/header.php
@@ -1,18 +1,21 @@
 <header class="banner">
     <div class="container">
-        <a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>">
-            <?php if ( get_theme_mod( 'site_logo' ) ) { ?>
+        <?php if ( get_theme_mod( 'site_logo' ) ) { ?>
+            <a class="header-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' );
+            ?>">
                 <img src="<?php echo get_theme_mod( 'site_logo' ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-            <?php } else {
-				bloginfo( 'name' );
-            } ?>
-        </a>
+            </a>
+        <?php } else { ?>
+            <a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>">
+                <span><?php bloginfo( 'name' ); ?></span>
+            </a>
+        <?php } ?>
         <nav class="nav-primary">
             <?php
-			if ( has_nav_menu( 'primary_navigation' ) ) :
-				wp_nav_menu( [ 'theme_location' => 'primary_navigation', 'menu_class' => 'nav' ] );
-			endif;
-			?>
+            if ( has_nav_menu( 'primary_navigation' ) ) :
+                wp_nav_menu( [ 'theme_location' => 'primary_navigation', 'menu_class' => 'nav' ] );
+            endif;
+            ?>
         </nav>
         <div class="header-search-wrapper"><?php get_search_form(); ?></div>
     </div>
