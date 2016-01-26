664973774687675e5bb2d7903976c3339826b46d
Faishal Saiyed <saiyedfaishal@gmail.com>
2016-01-26 07:15:06 +0530

    add link into feature image

--- templates/content.php
+++ templates/content.php
@@ -5,12 +5,15 @@
 		</div>
 	<?php } ?>
 	<header>
-		<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<h2 class="entry-title"><a href="<?php the_permalink(); ?>"
+		                           title="<?php echo get_the_title(); ?>"><?php the_title(); ?></a></h2>
 	</header>
 	<?php if ( has_post_thumbnail() ) { ?>
-		<div class="post-feature-image"><?php the_post_thumbnail( 'full' ); ?></div>
+		<a href="<?php the_permalink(); ?>" title="<?php echo get_the_title(); ?>">
+			<div class="post-feature-image"><?php the_post_thumbnail( 'full' ); ?></div>
+		</a>
 		<?php
-} ?>
+	} ?>
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div>
