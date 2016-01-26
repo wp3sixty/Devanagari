75419c47f498b16c6788213d5a594586f44abcdd
Faishal Saiyed <saiyedfaishal@gmail.com>
2016-01-26 06:44:36 +0530

    Fix single view issues

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,23 +1,25 @@
 <?php while ( have_posts() ) : the_post(); ?>
 	<article <?php post_class(); ?>>
-		<div class="entry-author">
-			<?php get_template_part( 'templates/entry-author' ); ?>
-		</div>
-		<header>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-		</header>
+		<?php if ( get_post_type() === 'post' ) { ?>
+			<div class="entry-author">
+				<?php get_template_part( 'templates/entry-author' ); ?>
+			</div>
+		<?php } ?>
 		<?php if ( has_post_thumbnail() ) { ?>
 			<div class="post-feature-image"><?php the_post_thumbnail( 'full' ); ?></div>
 			<?php
-} ?>
+		} ?>
+		<header>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</header>
 		<div class="entry-content">
 			<?php the_content(); ?>
 		</div>
 		<footer>
 			<?php wp_link_pages( [
-				'before' => '<nav class="page-nav"><p>' . __( 'Pages:', 'devanagari' ),
-				'after'  => '</p></nav>',
-			] ); ?>
+				                     'before' => '<nav class="page-nav"><p>' . __( 'Pages:', 'devanagari' ),
+				                     'after'  => '</p></nav>',
+			                     ] ); ?>
 		</footer>
 		<div class="tag-container">
 			<?php the_tags( '', '' ); ?>
--- templates/header.php
+++ templates/header.php
@@ -13,7 +13,7 @@
         <nav class="nav-primary">
             <?php
             if ( has_nav_menu( 'primary_navigation' ) ) :
-                wp_nav_menu( [ 'theme_location' => 'primary_navigation', 'menu_class' => 'nav' ] );
+                wp_nav_menu( [ 'theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'depth'=> 1 ] );
             endif;
             ?>
         </nav>
